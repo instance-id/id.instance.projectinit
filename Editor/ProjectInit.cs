c2b8d65f72aa8b67b3cb4b2c29c4bbb6c2afc5a5
instance.id <dan@instance.id>
2021-05-21 19:33:02 -0500

    Added comments

--- Editor/ProjectInit.cs
+++ Editor/ProjectInit.cs
@@ -6,18 +6,23 @@ public static class ProjectInit
 {
     static ProjectInit()
     {
+        // --|----------------------------------------------------- Set scripting backend to IL2CPP
         if (PlayerSettings.GetScriptingBackend(BuildTargetGroup.Standalone) != ScriptingImplementation.IL2CPP)
             PlayerSettings.SetScriptingBackend(BuildTargetGroup.Standalone, ScriptingImplementation.IL2CPP);
         
+        // --|------------------------------------------------------- Set scripting API to .Net 4.6
         if (PlayerSettings.GetApiCompatibilityLevel(BuildTargetGroup.Standalone) != ApiCompatibilityLevel.NET_4_6)
             PlayerSettings.SetApiCompatibilityLevel(BuildTargetGroup.Standalone, ApiCompatibilityLevel.NET_4_6);
 
+        // --|---------------------- Disable the 'Use Default Graphics API option for Linux targets
         if (PlayerSettings.GetUseDefaultGraphicsAPIs(BuildTarget.StandaloneLinux64) == true)
             PlayerSettings.SetUseDefaultGraphicsAPIs(BuildTarget.StandaloneLinux64, false);
 
+        // --|------------------------------------ Remove the OpenGL graphics API and set to Vulkan
         if (PlayerSettings.GetGraphicsAPIs(BuildTarget.StandaloneLinux64).Length > 1)
             PlayerSettings.SetGraphicsAPIs(BuildTarget.StandaloneLinux64, new[] {GraphicsDeviceType.Vulkan});
 
+        // --|------ Enable the use of unsafe code, enable incremental GC, and disable auto-refresh
         if (!PlayerSettings.allowUnsafeCode) PlayerSettings.allowUnsafeCode = true;
         if (!PlayerSettings.gcIncremental) PlayerSettings.gcIncremental = true;
         if (EditorPrefs.GetBool("kAutoRefresh", true)) EditorPrefs.SetBool("kAutoRefresh", false);
